c3d9cd01b9eefa30678c893608881a800cf4ecec
Muhammed Toprak <muhammedtoprak@live.com>
2025-11-08 23:10:40 +0300

    Implement moveCall in createArena function for arena creation logic

--- ui/src/utility/arena/create_arena.ts
+++ ui/src/utility/arena/create_arena.ts
@@ -2,13 +2,19 @@ import { Transaction } from "@mysten/sui/transactions";
 
 export const createArena = (packageId: string, heroId: string) => {
   const tx = new Transaction();
-  
+
+  tx.moveCall({
+    target: `${packageId}::arena::create_arena`,
+    arguments: [tx.object(heroId)],
+    typeArguments: [],
+  });
+
   // TODO: Add moveCall to create a battle place
   // Function: `${packageId}::arena::create_arena`
   // Arguments: heroId (object)
-    // Hints:
-    // Use tx.object() for the hero object
-    // This creates a shared object that others can battle against
-  
+  // Hints:
+  // Use tx.object() for the hero object
+  // This creates a shared object that others can battle against
+
   return tx;
 };
